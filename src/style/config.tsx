dd7f5f61958a0defcc338e313eed2878e5f70ac8
whdgur5717 <whdgur57117@gmail.com>
2024-12-02 21:50:51 +0900

    feat: antd theme config 설정

---
+++ src/style/config.tsx
@@ -0,0 +1,17 @@
+import { type ThemeConfig } from "antd";
+
+export const config: ThemeConfig = {
+  token: {
+    colorPrimary: "#4a7cfe",
+    colorInfo: "#4a7cfe",
+    colorBorder: "#e3e3e3",
+    colorFillAlter: "rgba(0,0,0,0.02)",
+    colorIcon: "rgba(0,0,0,0.45)",
+    colorSplit: "rgba(0,0,0,0.06)",
+  },
+  components: {
+    Button: {
+      colorBgTextHover: "rgba(0,0,0,0.06)",
+    },
+  },
+};
