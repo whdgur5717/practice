b5341733c07cecf7672b37b9e9bb86de39ad842e
whdgur5717 <whdgur57117@gmail.com>
2024-12-03 20:44:02 +0900

    refactor: 필드 타입 개선 - 타입 정의 대신 객체의 as const 키워드 사용

--- src/entities/userTable/model.ts
+++ src/entities/userTable/model.ts
@@ -1,44 +1,60 @@
 type FieldType = "text" | "textarea" | "date" | "select" | "checkbox";
 
-type BaseField = {
+type MinimumField = {
+  type: FieldType;
   label: string;
   required: boolean;
 };
 
-type Field =
-  | (BaseField & { type: Exclude<FieldType, "select"> })
-  | (BaseField & { type: "select"; options: string[] });
+const nameField = {
+  type: "text" as const,
+  label: "이름" as const,
+  required: true as const,
+  defaultValue: "",
+};
 
-export const Fields = [
-  {
-    type: "text",
-    label: "이름",
-    required: true,
-  },
-  {
-    type: "text",
-    label: "주소",
-    required: false,
-  },
-  {
-    type: "textarea",
-    label: "메모",
-    required: false,
-  },
-  {
-    type: "date",
-    label: "가입일",
-    required: true,
-  },
-  {
-    type: "select",
-    label: "직업",
-    required: false,
-    options: ["개발자", "PO", "디자이너"],
-  },
-  {
-    type: "checkbox",
-    label: "이메일 수신 동의",
-    required: false,
-  },
-] as const satisfies Field[];
+const addressField = {
+  type: "text" as const,
+  label: "주소" as const,
+  required: false as const,
+  defaultValue: "",
+};
+
+const memoField = {
+  type: "textarea" as const,
+  label: "메모" as const,
+  required: false as const,
+  defaultValue: "",
+};
+
+const dateField = {
+  type: "date" as const,
+  label: "가입일" as const,
+  required: true as const,
+  defaultValue: "",
+};
+
+const jobOptions = ["개발자", "PO", "디자이너"] as const;
+
+const jobField = {
+  type: "select" as const,
+  label: "직업" as const,
+  required: false as const,
+  options: jobOptions,
+  defaultValue: "개발자" as (typeof jobOptions)[number],
+};
+
+const emailField = {
+  type: "checkbox" as const,
+  label: "이메일 수신 동의" as const,
+  required: false as const,
+  defaultValue: false,
+};
+
+type Form =
+  | typeof nameField
+  | typeof addressField
+  | typeof memoField
+  | typeof dateField
+  | typeof jobField
+  | typeof emailField;
