f9bffcd6f630edc19bbe848b820a4e8b50ff30c5
whdgur5717 <whdgur57117@gmail.com>
2024-12-03 23:09:26 +0900

    feat: dataSource, columns 데이터 추가

--- src/Page/ui/UserTable.tsx
+++ src/Page/ui/UserTable.tsx
@@ -1,5 +1,27 @@
 import { Table } from "antd";
+import type { TableColumnsType } from "antd";
+import {
+  fields,
+  initialUserRecords,
+  type UserRecord,
+} from "../../entities/userTable/model";
+
+interface DataType extends UserRecord {
+  key: React.Key;
+}
+
+const columns: TableColumnsType<DataType> = fields.map((field) => {
+  return {
+    title: field.label,
+    dataIndex: field.label,
+  };
+});
+
+const dataSources: DataType[] = initialUserRecords.map((record, index) => ({
+  ...record,
+  key: index + 1,
+}));
 
 export const UserTable = () => {
-  return <Table />;
+  return <Table<DataType> dataSource={dataSources} columns={columns} />;
 };
