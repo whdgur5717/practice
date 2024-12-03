72265c6bbed99ef1e9b5419121b55a4cabf446b0
whdgur5717 <whdgur57117@gmail.com>
2024-12-03 21:21:18 +0900

    feat: 초기 레코드 데이터 추가

--- src/entities/userTable/model.ts
+++ src/entities/userTable/model.ts
@@ -76,3 +76,22 @@ type UserRecord = {
     { label: K }
   >["defaultValue"];
 };
+
+export const initialUserRecords = [
+  {
+    이름: "John Doe",
+    주소: "서울 강남구",
+    메모: "외국인",
+    가입일: "2024-10-02",
+    직업: "개발자",
+    "이메일 수신 동의": true,
+  },
+  {
+    이름: "Foo Bar",
+    주소: "서울 서초구",
+    메모: "한국인",
+    가입일: "2024-10-01",
+    직업: "PO",
+    "이메일 수신 동의": false,
+  },
+] satisfies UserRecord[];
