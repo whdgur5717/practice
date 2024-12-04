926728e4de3215c188999838c4d0bd81a29058b5
whdgur5717 <whdgur57117@gmail.com>
2024-12-04 22:05:13 +0900

    feat: requiredMark 커스텀 컴포넌트 추가

--- src/Page/ui/UserTableForm.tsx
+++ src/Page/ui/UserTableForm.tsx
@@ -4,29 +4,56 @@ import { useTheme } from "antd-style";
 
 type FieldType = UserRecord;
 
+export const FormLabel = ({ children }: { children: React.ReactNode }) => {
+  const token = useTheme();
+
+  return (
+    <Typography.Text
+      strong
+      style={{
+        color: token.colorTextTertiary,
+        fontSize: token.fontSizeLG,
+        lineHeight: token.lineHeightLG,
+      }}
+    >
+      {children}
+    </Typography.Text>
+  );
+};
+
+const RequiredMark = (
+  label: React.ReactNode,
+  { required }: { required: boolean }
+) => {
+  const token = useTheme();
+
+  return (
+    <span style={{ display: "inline-flex", gap: token.marginXXS }}>
+      {label}
+      {required && (
+        <span style={{ color: token.colorError, fontSize: token.fontSizeLG }}>
+          *
+        </span>
+      )}
+    </span>
+  );
+};
+
 export const UserTableForm = () => {
   const token = useTheme();
 
   return (
-    <Form layout="vertical" style={{ width: "100%" }}>
+    <Form
+      layout="vertical"
+      style={{ width: "100%" }}
+      requiredMark={RequiredMark}
+    >
       {fields.map((field) => {
         return (
           <Form.Item<FieldType>
-            label={
-              <Typography.Text
-                strong
-                style={{
-                  color: token.colorTextTertiary,
-                  fontSize: token.fontSizeLG,
-                  lineHeight: token.lineHeightLG,
-                }}
-              >
-                {field.label}
-              </Typography.Text>
-            }
+            label={<FormLabel>{field.label}</FormLabel>}
             name={field.label}
             key={field.label}
-            required={field.required}
             rules={[
               {
                 required: field.required,
