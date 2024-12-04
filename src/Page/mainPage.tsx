6895075e8a412c6b5fb5ae87c2c0f799d03725b4
whdgur5717 <whdgur57117@gmail.com>
2024-12-04 19:47:35 +0900

    refactor: 데이터 전달 및 책임을 고려하여 구조 변경 및 컴포넌트 분리

--- src/Page/mainPage.tsx
+++ src/Page/mainPage.tsx
@@ -1,9 +1,37 @@
+import { Button, Flex, Typography } from "antd";
 import { UserTable } from "./ui/UserTable";
+import { PlusOutlined } from "@ant-design/icons";
+import type { DataType } from "./ui/type";
+import { initialUserRecords } from "../entities/userTable/model";
+import { useState } from "react";
+import { createColumns } from "./ui/columns";
+
+const initialDataSources: DataType[] = initialUserRecords.map(
+  (record, index) => ({
+    ...record,
+    key: index + 1,
+  })
+);
 
 const MainPage = () => {
+  const [tableData, setTableData] = useState(initialDataSources);
+
   return (
     <div>
-      <UserTable />
+      <Flex
+        justify="space-between"
+        align="center"
+        style={{ padding: "8px 14px" }}
+      >
+        <Typography.Title level={5}>회원 목록</Typography.Title>
+        <Button type="primary" icon={<PlusOutlined />}>
+          추가
+        </Button>
+      </Flex>
+      <UserTable<DataType>
+        dataSource={tableData}
+        columns={createColumns(tableData)}
+      />
     </div>
   );
 };
