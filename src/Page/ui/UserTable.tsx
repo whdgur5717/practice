12fcdda997c7b0c292dfc607c91b6dc51aad8243
whdgur5717 <whdgur57117@gmail.com>
2024-12-05 03:43:11 +0900

    refactor: 불필요한 데이터 코드 삭제

--- src/Page/ui/UserTable.tsx
+++ src/Page/ui/UserTable.tsx
@@ -1,14 +1,4 @@
 import { Table, type TableProps } from "antd";
-import { initialUserRecords } from "../../entities/userTable/model";
-import { useState } from "react";
-import type { DataType } from "./type";
-
-const initialDataSources: DataType[] = initialUserRecords.map(
-  (record, index) => ({
-    ...record,
-    key: index + 1,
-  })
-);
 
 interface UserTableProps<T> {
   dataSource: TableProps<T>["dataSource"];
@@ -16,8 +6,6 @@ interface UserTableProps<T> {
 }
 
 export const UserTable = <T,>({ dataSource, columns }: UserTableProps<T>) => {
-  const [tableData, setTableData] = useState(initialDataSources);
-
   return (
     <Table<T>
       dataSource={dataSource}
